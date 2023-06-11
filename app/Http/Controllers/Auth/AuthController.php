7630ad80717146219aa559ebbd1c7ee2c3e17671
infsolution <clsinfsolution@gmail.com>
2023-06-11 18:36:43 -0300

    updaate passport

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request){
+        $credentials = $request->only(['email', 'password']);
+        Auth::attempt($credentials);
+        $user = Auth::user();
+        if(!$user){
+            return response(['message'=>'unaltorized'], 401);
+        }
+        $token = $user->createToken('userToken')->accessToken;
+        return response(['access_token'=>$token], 200);
+    }
 }
